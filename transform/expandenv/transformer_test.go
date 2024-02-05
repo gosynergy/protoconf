d3ab8a0c972ab964007c26e9285a58cc8ca23657
Trần Công <trancong12102@gmail.com>
2024-02-05 14:25:01 +0700

    chore(test): test expand env with default os.Getenv
    Signed-off-by: Trần Công <trancong12102@gmail.com>

--- transform/expandenv/transformer_test.go
+++ transform/expandenv/transformer_test.go
@@ -10,32 +10,41 @@ import (
 func TestExpandEnvTransformer_Transform(t *testing.T) {
 	t.Parallel()
 
-	t.Run("success", func(t *testing.T) {
-		t.Parallel()
+	envs := map[string]string{
+		"TEST_ENV":   "test value",
+		"TEST_ENV_1": "test value 1",
+	}
 
-		envs := map[string]string{
-			"TEST_ENV":   "test value",
-			"TEST_ENV_1": "test value 1",
-		}
+	transformer := NewTransformer(WithGetenv(func(s string) string {
+		return envs[s]
+	}))
+	values := map[string]interface{}{
+		"test": "${TEST_ENV}",
+		"a": []interface{}{
+			"test",
+			"${TEST_ENV_1}",
+		},
+	}
 
-		transformer := NewTransformer(WithGetenv(func(s string) string {
-			return envs[s]
-		}))
-		values := map[string]interface{}{
-			"test": "${TEST_ENV}",
-			"a": []interface{}{
-				"test",
-				"${TEST_ENV_1}",
-			},
-		}
+	expanded, err := transformer.Transform(values)
+	require.NoError(t, err)
+	assert.Equal(t, "test value", expanded["test"])
 
-		expanded, err := transformer.Transform(values)
-		require.NoError(t, err)
-		assert.Equal(t, "test value", expanded["test"])
+	a, ok := expanded["a"].([]interface{})
+	require.True(t, ok)
+	assert.Equal(t, "test", a[0])
+	assert.Equal(t, "test value 1", a[1])
+}
 
-		a, ok := expanded["a"].([]interface{})
-		require.True(t, ok)
-		assert.Equal(t, "test", a[0])
-		assert.Equal(t, "test value 1", a[1])
-	})
+func TestExpandEnvTransformer_Transform_WithDefaultOsGetenv(t *testing.T) {
+	t.Parallel()
+
+	transformer := NewTransformer()
+	values := map[string]interface{}{
+		"test": "${TEST_ENV}",
+	}
+
+	expanded, err := transformer.Transform(values)
+	require.NoError(t, err)
+	assert.Empty(t, expanded["test"])
 }
